4fdb8586eccd3de180181466545ee07c290ae025
GanguChirudeep <144471481+GanguChirudeep@users.noreply.github.com>
2023-11-05 20:52:32 +0530

    Update Lab6-ML.py

--- Lab6-ML.py
+++ Lab6-ML.py
@@ -65,5 +65,32 @@ plt.ylabel('embed_1')
 plt.show()
 
 
+# In[41]:
+
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Define the data
+
+
+# Label encoding for 'Class' column
+df['Label'] = df['Label'].astype('category')
+df['Label'] = df['Label'].cat.codes
+
+# Split the data into training and test sets
+X = df[['embed_0', 'embed_1']]
+y = df['Label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a logistic regression model and fit it to the training data
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Predict the classes on the test data and evaluate the accuracy
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
 
 
